4749c32e5d677b915e01d5b31491a643a49e87eb
Daniel <espindola.daniel@outlook.com.br>
2022-08-18 17:01:03 -0300

    desenhando os ''inimigos'' na tela

--- src/Main.hs
+++ src/Main.hs
@@ -11,14 +11,23 @@ alturaPlayer = 30
 
 larguraBola = 15
 
+larguraParede = 50
+alturaParede = 20
+
 type Player = (Float, Float, Float) -- X, Y, VEL
 
 type Bola = (Float, Float, Float, Float) -- X, Y, vx, vy
 
-type Mundo = (Player, Bola)
+type Parede = (Float, Float) -- X, Y, vx, vy
+
+type Mundo = (Player, Bola, [Parede])
 
 inicializaMundo :: Mundo
-inicializaMundo = ((0, -alturaTela/2 + 50, 0), (0, 0, -200, 200))
+inicializaMundo = (newPlayer, bola, paredes)
+  where
+    newPlayer = (0, -alturaTela/2 + 50, 0)
+    bola = (0, 0, -200, 200)
+    paredes = [(x * (larguraParede + 10), alturaTela/2 - 120 + (alturaParede + 5) * y) | x <- [-5 .. 5], y <- [-3 .. 3]]
 
 desenhaPlayer :: Player -> Picture
 desenhaPlayer (x, y, _) = color red $ translate x y $ rectangleSolid larguraPlayer alturaPlayer
@@ -26,18 +35,26 @@ desenhaPlayer (x, y, _) = color red $ translate x y $ rectangleSolid larguraPlay
 desenhaBola :: Bola -> Picture
 desenhaBola (x, y, _, _) = color green $ translate x y $ circleSolid larguraBola
 
-desenhaMundo (player, bola) = Pictures [playerPic, bolaPic]
+desenhaParede :: Parede -> Picture
+desenhaParede (px, py) = color yellow $ translate px py $ rectangleSolid larguraParede alturaParede
+
+desenhaParedes :: [Parede] -> Picture
+desenhaParedes ps = pictures $ map desenhaParede ps
+
+desenhaMundo (player, bola, ps) = Pictures [playerPic, bolaPic, paredesPic]
   where
     playerPic = desenhaPlayer player
     bolaPic = desenhaBola bola
+    paredesPic = desenhaParedes ps
 
 inputHandler :: Event -> Mundo -> Mundo
-inputHandler (EventKey (SpecialKey KeyRight) Down _ _) ((x, y, v), b) = ((x, y, 200), b)
-inputHandler (EventKey (SpecialKey KeyRight) Up _ _) ((x, y, v), b) = ((x, y, 0), b)
-inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) ((x, y, v), b) = ((x, y, -200), b)
-inputHandler (EventKey (SpecialKey KeyLeft) Up _ _) ((x, y, v), b) = ((x, y, 0), b)
+inputHandler (EventKey (SpecialKey KeyRight) Down _ _) ((x, y, v), b, ps) = ((x, y, 200), b, ps)
+inputHandler (EventKey (SpecialKey KeyRight) Up _ _) ((x, y, v), b, ps) = ((x, y, 0), b, ps)
+inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) ((x, y, v), b, ps) = ((x, y, -200), b, ps)
+inputHandler (EventKey (SpecialKey KeyLeft) Up _ _) ((x, y, v), b, ps) = ((x, y, 0), b, ps)
 inputHandler _ m = m
 
+atualizaPosicaoPlayer :: Float -> Player -> Player
 atualizaPosicaoPlayer dt (x, y, vel) = (xres, y, vel)
   where
     dx = vel * dt
@@ -55,7 +72,7 @@ colidiuPlayer (xp, yp) (xb, yb) =
     dentroH = xb > xp - larguraPlayer/2 && xb < xp + larguraPlayer/2
     dentroY = yb < comecoYPlayer && yb > fimYPlayer
 
-atualizaPosicaoBola dt (p@(xp, yp, _), (x, y, vx, vy)) = (p, (x1, y1, vxRes, vyRes))
+atualizaPosicaoBola dt (p@(xp, yp, _), (x, y, vx, vy), ps) = (p, (x1, y1, vxRes, vyRes), ps)
   where
     dx = vx * dt
     dy = vy * dt
@@ -63,18 +80,18 @@ atualizaPosicaoBola dt (p@(xp, yp, _), (x, y, vx, vy)) = (p, (x1, y1, vxRes, vyR
     y1 = y + dy
     cl = colidiuPlayer (xp, yp) (x, y)
     vxRes
-      | (x1 > larguraTela / 2 - larguraBola/2) || (cl && vx < 0) = -200 
+      | (x1 > larguraTela / 2 - larguraBola/2) || (cl && vx < 0) = -200
       | (x1 < -larguraTela / 2 + larguraBola/2) || (cl && vx > 0) = 200
       | otherwise = vx
     vyRes
-      | (y1 > alturaTela / 2 - larguraBola/2) = -200 
+      | y1 > alturaTela / 2 - larguraBola/2 = -200
       | cl = 200
       | otherwise = vy
 
 atualizaMundo :: Float -> Mundo -> Mundo
-atualizaMundo dt (player, bola) = m2
+atualizaMundo dt (player, bola, paredes) = m2
   where
-    m1 = (atualizaPosicaoPlayer dt player, bola)
+    m1 = (atualizaPosicaoPlayer dt player, bola, paredes)
     m2 = atualizaPosicaoBola dt m1
 
 main :: IO ()
