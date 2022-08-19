e7a3140e114a9ba54ad40def83ddefd80bea5e35
Daniel <espindola.daniel@outlook.com.br>
2022-08-19 07:16:42 -0300

    Adicionado status no game, com as respectivas telas de vitória/derrota/start etc

--- src/Main.hs
+++ src/Main.hs
@@ -2,32 +2,85 @@ module Main (main) where
 
 import Graphics.Gloss.Interface.Pure.Game
 
-larguraTela, alturaTela :: Float
+larguraTela, alturaTela, larguraPlayer, alturaPlayer, larguraBola, larguraParede, alturaParede :: Float
 larguraTela = 800
 alturaTela = 600
-
 larguraPlayer = 120
 alturaPlayer = 30
-
 larguraBola = 15
-
 larguraParede = 50
 alturaParede = 20
+velBola = 280
+velPlayer = 300
 
 type Player = (Float, Float, Float) -- X, Y, VEL
 
 type Bola = (Float, Float, Float, Float) -- X, Y, vx, vy
 
-type Parede = (Float, Float) -- X, Y, vx, vy
+type Parede = (Float, Float) -- X, Y
 
-type Mundo = (Player, Bola, [Parede])
+type Mundo = (Player, Bola, [Parede], Status)
+
+data Status = 
+    Start 
+  | Playing 
+  | Lose 
+  | Win 
+  deriving (Eq)
+
+mundoStart :: Mundo
+mundoStart = ((0, 0, 0), (0, 0, 0, 0), [], Start)
 
 inicializaMundo :: Mundo
-inicializaMundo = (newPlayer, bola, paredes)
+inicializaMundo =
+    (newPlayer,
+    bola,
+    paredes,
+    Playing)
   where
     newPlayer = (0, -alturaTela/2 + 50, 0)
-    bola = (0, 0, -200, 200)
+    bola = (0, 0, -velBola, velBola)
     paredes = [(x * (larguraParede + 10), alturaTela/2 - 120 + (alturaParede + 5) * y) | x <- [-5 .. 5], y <- [-3 .. 3]]
+
+desenhaTelaVitoria :: Picture
+desenhaTelaVitoria =
+  Pictures
+    [ Color white $ Translate posXTitulo posYTitulo $ Scale (tamTitulo * 2) (tamTitulo * 2) $ Text "YOU WIN!",
+      Color white $ Translate posXTitulo (posYTitulo - 100) $ Scale tamTitulo tamTitulo $ Text "SCORE: 123450 !!",
+      Color white $ Translate posXTitulo (posYTitulo - 200) $ Scale tamTitulo tamTitulo $ Text "HIGHEST SCORE: 99999999999999",
+      Color white $ Translate posXTitulo (posYTitulo - 350) $ Scale tamTitulo tamTitulo $ Text "Aperte Espaco para jogar novamente"
+    ]
+  where
+    tamTitulo = 0.25
+    posXTitulo = -350
+    posYTitulo = alturaTela / 2 - 100
+
+desenhaTelaDerrota :: Picture
+desenhaTelaDerrota =
+  Pictures
+    [ Color white $ Translate posXTitulo posYTitulo $ Scale (tamTitulo * 2) (tamTitulo * 2) $ Text "YOU LOSE!",
+      Color white $ Translate posXTitulo (posYTitulo - 100) $ Scale tamTitulo tamTitulo $ Text "SCORE: 123450 !!",
+      Color white $ Translate posXTitulo (posYTitulo - 200) $ Scale tamTitulo tamTitulo $ Text "HIGHEST SCORE: 99999999999999",
+      Color white $ Translate posXTitulo (posYTitulo - 350) $ Scale tamTitulo tamTitulo $ Text "Aperte Espaco para jogar novamente"
+    ]
+  where
+    tamTitulo = 0.25
+    posXTitulo = -350
+    posYTitulo = alturaTela / 2 - 100
+
+desenhaSplashScreen :: Picture
+desenhaSplashScreen =
+  Pictures
+    [ Color white $ Translate (posXTitulo + 70) posYTitulo $ Scale (tamTitulo * 2) (tamTitulo * 2) $ Text "Haskell Breakout!",
+      Color white $ Translate (posXTitulo + 200) (posYTitulo - 100) $ Scale tamTitulo tamTitulo $ Text "Controles:",
+      Color white $ Translate posXTitulo (posYTitulo - 200) $ Scale tamTitulo tamTitulo $ Text "'A'/'S' e Esquerda/Direita movem o jogador",
+      Color white $ Translate posXTitulo (posYTitulo - 250) $ Scale tamTitulo tamTitulo $ Text "Espaco Atira",
+      Color white $ Translate posXTitulo (posYTitulo - 350) $ Scale tamTitulo tamTitulo $ Text "Aperte Espaco para comecar"
+    ]
+  where
+    tamTitulo = 0.25
+    posXTitulo = -350
+    posYTitulo = alturaTela / 2 - 100
 
 desenhaPlayer :: Player -> Picture
 desenhaPlayer (x, y, _) = color red $ translate x y $ rectangleSolid larguraPlayer alturaPlayer
@@ -41,17 +94,33 @@ desenhaParede (px, py) = color yellow $ translate px py $ rectangleSolid largura
 desenhaParedes :: [Parede] -> Picture
 desenhaParedes ps = pictures $ map desenhaParede ps
 
-desenhaMundo (player, bola, ps) = Pictures [playerPic, bolaPic, paredesPic]
+desenhaMundo :: Mundo -> Picture
+desenhaMundo (player, bola, ps, status) =
+  case status of
+    Start -> desenhaSplashScreen
+    Playing -> Pictures [playerPic, bolaPic, paredesPic]
+    Lose -> desenhaTelaDerrota
+    Win -> desenhaTelaVitoria
   where
     playerPic = desenhaPlayer player
     bolaPic = desenhaBola bola
     paredesPic = desenhaParedes ps
 
 inputHandler :: Event -> Mundo -> Mundo
-inputHandler (EventKey (SpecialKey KeyRight) Down _ _) ((x, y, v), b, ps) = ((x, y, 200), b, ps)
-inputHandler (EventKey (SpecialKey KeyRight) Up _ _) ((x, y, v), b, ps) = ((x, y, 0), b, ps)
-inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) ((x, y, v), b, ps) = ((x, y, -200), b, ps)
-inputHandler (EventKey (SpecialKey KeyLeft) Up _ _) ((x, y, v), b, ps) = ((x, y, 0), b, ps)
+inputHandler (EventKey (SpecialKey KeyRight) Down _ _) ((x, y, v), b, ps, s) = ((x, y, velPlayer), b, ps, s)
+inputHandler (EventKey (SpecialKey KeyRight)   Up _ _) ((x, y, v), b, ps, s) = ((x, y, 0), b, ps, s)
+inputHandler (EventKey (SpecialKey KeyLeft)  Down _ _) ((x, y, v), b, ps, s) = ((x, y, -velPlayer), b, ps, s)
+inputHandler (EventKey (SpecialKey KeyLeft)    Up _ _) ((x, y, v), b, ps, s) = ((x, y, 0), b, ps, s)
+inputHandler (EventKey (SpecialKey KeySpace)   Up _ _) ((x, y, v), b, ps, s) =
+  case s of
+    Start -> inicializaMundo
+    Lose -> inicializaMundo
+    Win -> inicializaMundo
+    _ -> ((x, y, 0), b, ps, s)
+inputHandler (EventKey (Char 'a')            Down _ _) ((x, y, v), b, ps, s) = ((x, y, velPlayer), b, ps, s)
+inputHandler (EventKey (Char 'a')              Up _ _) ((x, y, v), b, ps, s) = ((x, y, 0), b, ps, s)
+inputHandler (EventKey (Char 'd')            Down _ _) ((x, y, v), b, ps, s) = ((x, y, -velPlayer), b, ps, s)
+inputHandler (EventKey (Char 'd')              Up _ _) ((x, y, v), b, ps, s) = ((x, y, 0), b, ps, s)
 inputHandler _ m = m
 
 atualizaPosicaoPlayer :: Float -> Player -> Player
@@ -64,6 +133,7 @@ atualizaPosicaoPlayer dt (x, y, vel) = (xres, y, vel)
       | x1 < -larguraTela/2 + larguraPlayer/2 = -larguraTela/2 + larguraPlayer/2
       | otherwise = x1
 
+colidiuPlayer :: (Float, Float) -> (Float, Float) -> Bool
 colidiuPlayer (xp, yp) (xb, yb) =
   dentroH && dentroY
   where
@@ -72,7 +142,8 @@ colidiuPlayer (xp, yp) (xb, yb) =
     dentroH = xb > xp - larguraPlayer/2 && xb < xp + larguraPlayer/2
     dentroY = yb < comecoYPlayer && yb > fimYPlayer
 
-atualizaPosicaoBola dt (p@(xp, yp, _), (x, y, vx, vy), ps) = (p, (x1, y1, vxRes, vyRes), ps)
+atualizaPosicaoBola :: Float -> Mundo -> Mundo
+atualizaPosicaoBola dt (p@(xp, yp, _), (x, y, vx, vy), ps, s) = (p, (x1, y1, vxRes, vyRes), ps, s)
   where
     dx = vx * dt
     dy = vy * dt
@@ -80,28 +151,37 @@ atualizaPosicaoBola dt (p@(xp, yp, _), (x, y, vx, vy), ps) = (p, (x1, y1, vxRes,
     y1 = y + dy
     cl = colidiuPlayer (xp, yp) (x, y)
     vxRes
-      | (x1 > larguraTela / 2 - larguraBola/2) || (cl && vx < 0) = -200
-      | (x1 < -larguraTela / 2 + larguraBola/2) || (cl && vx > 0) = 200
+      | (x1 > larguraTela / 2 - larguraBola/2) || (cl && vx < 0) = -velBola
+      | (x1 < -larguraTela / 2 + larguraBola/2) || (cl && vx > 0) = velBola
       | otherwise = vx
     vyRes
-      | y1 > alturaTela / 2 - larguraBola/2 = -200
-      | cl = 200
+      | y1 > alturaTela / 2 - larguraBola/2 = -velBola
+      | cl = velBola
       | otherwise = vy
 
 atualizaMundo :: Float -> Mundo -> Mundo
-atualizaMundo dt (player, bola, paredes) = m2
+atualizaMundo dt (player, bola, paredes, s) = m3
   where
-    m1 = (atualizaPosicaoPlayer dt player, bola, paredes)
+    m1 = (atualizaPosicaoPlayer dt player, bola, paredes, s)
     m2 = atualizaPosicaoBola dt m1
+    m3 = atualizaStatusGame m2
+
+atualizaStatusGame :: Mundo -> Mundo
+atualizaStatusGame (p, b@(x, y, vx, vy), ps, s) = mundoRes
+  where
+    novoMundo = inicializaMundo
+    mundoRes
+      | s == Playing && null ps = (p, b, ps, Win)
+      | s == Playing && (y < -alturaTela/2 - 100) = (p, b, ps, Lose)
+      | otherwise = (p, b, ps, s)
 
 main :: IO ()
 main = do
-
   play
     janela
     black
     60
-    inicializaMundo
+    mundoStart
     desenhaMundo
     inputHandler
     atualizaMundo
